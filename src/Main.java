e6562a61bd26b4b616d01450e4cb28b0ce7b78a5
unknown <simonchitlango1@gmail.com>
2024-05-10 14:18:17 +0200

    First Change

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        Scanner input = new Scanner(Systemin);
+        System.out.print("Hello world!");
+        Scanner input = new Scanner(System.in);
         System.out.print("your name");
         String name=input.next();
-        System.out.println("welcome"+name);
-        git add .
-        git commit -m "First change"
-            git push
+        System.out.print("welcome"+name);
+
     }
 }
